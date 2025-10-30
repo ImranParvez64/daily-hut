d16bd0e26507bb8c16a202601a8595ed3a35753f
unknown <you@example.com>
2025-10-30 10:29:47 +0600

    ffd

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   images: {
-    domains: ['i.ibb.co.com','nest-frontend-v6.vercel.app','nest-frontend-v6.vercel.app'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'nest-frontend-v6.vercel.app',
+      },
+    ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
