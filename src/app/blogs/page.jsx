0c75a41f4f4ab57bdbb461752bb90d7569aadddc
unknown <you@example.com>
2025-10-30 19:57:39 +0600

    ffsad

---
+++ src/app/blogs/page.jsx
@@ -0,0 +1,13 @@
+export const dynamic = "force-dynamic";
+
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            this is a blogs page
+        </div>
+    );
+};
+
+export default page;
